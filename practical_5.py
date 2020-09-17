9cf6137c7ba60ba28c3b80a81d39956e13a3e66d
bhavya1731 <60725749+bhavya1731@users.noreply.github.com>
2020-09-17 16:26:38 +0530

    Update practical_5.py

--- practical_5.py
+++ practical_5.py
@@ -20,7 +20,7 @@ if a == 'b':
                 else:
                     u = mid-1
         return False            
-    #list = [0,1,2,3,4,5,6,7,8,45,72]
+    
     list.sort()
     n= int(input("Enter the numbers for binary  search : "))
     if search(list, n):
