287a10f94b50d4354e4cf722cc18455aecf0768e
bhavya1731 <60725749+bhavya1731@users.noreply.github.com>
2020-10-26 15:55:29 +0530

    Update Practical 4.py

--- Practical 4.py
+++ Practical 4.py
@@ -1,35 +1,63 @@
 #Bhavya shah
 #SYCS
 #4072
-class Stack():
-    def __init__(self):
-        self.items = [1,2,3,4,5]
+class CircularQueue:
+  
 
-    
-    
-    def enque(self,item):
-        self.items.append(item)
-        print(item)
+  def __init__(self, maxSize):
+    self.queue = list()
+    self.maxSize = maxSize
+    self.head = 0
+    self.tail = 0
 
-        
-    def deque(self):
-        b= self.items
-        b.pop()
-        print(b)
+  
+  def enqueue(self, data):
+    if self.size() == (self.maxSize - 1):
+      return("Queue is full!")
+    else:
+        self.queue.append(data)
+     
+        self.tail = (self.tail+1) % self.maxSize
+        return True
+  
+  
+  def dequeue(self):
+    if self.size() == 0:
+      return("Queue is empty!")
+    else:
+        data = self.queue[self.head]
+      
+        self.head = (self.head+1) % self.maxSize
+        return data
+  
+  
+  def size(self):
+    if self.tail >= self.head:
+      qSize = self.tail - self.head
+    else:
+      qSize = self.maxSize - (self.head - self.tail)
+    return qSize
 
-    def traverse(self):
-        a = []
-        l = self.items
-        for i in l:
-            a.append(i)
-        print(a)
 
-s=Stack()
+size = input("Enter the size of the Circular Queue : ")
+q = CircularQueue(int(size))
 
-print("Adding the element in the queue : ")
-s.enque(6)
-print("initial queue : ")
-s.traverse()
 
-print("After removing an element from the queue : ")
-s.deque()
+print(q.enqueue(10))
+print(q.enqueue(20))
+print(q.enqueue(30))
+print(q.enqueue(40))
+print(q.enqueue(50))
+print(q.enqueue('Studytonight'))
+print(q.enqueue(70))
+print(q.enqueue(80))
+print(q.dequeue())
+print(q.dequeue())
+print(q.dequeue())
+print(q.dequeue())
+print(q.dequeue())
+print(q.dequeue())
+print(q.dequeue())
+print(q.dequeue())
+print(q.dequeue())
+
